30f9f2cb67e7da66c7d75976b5b13623ed0a9c3f
David Zambrano <davazamb10@outlook.com>
2017-02-21 21:11:45 -0400

    Created Project Asp.net MVC with WebAppi: Included SQL DataBase Azure in web.Config. connectionstring, Create First Table Vegetable with their dataannotations...

---
+++ VegetableAPI/Models/Vegetable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace VegetableAPI.Models
+{
+    public class Vegetable
+    {
+        [Key]
+        public int VegetableId { get; set; }
+
+        [Required(ErrorMessage = "You must enter a {0}")]
+        [StringLength(50, ErrorMessage = "The field {0} can contain maximun {1} and minimum {2} characters", MinimumLength = 1)]
+        [Index("Vegetable_Description_Index", IsUnique = true)]
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ VegetableAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VegetableAPI.Startup))]
+namespace VegetableAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
